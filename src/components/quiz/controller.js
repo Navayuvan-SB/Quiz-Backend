f089493217c33689b6f8be094b83adff426a11a5
Navayuvan <ns.navayuvan@gmail.com>
2021-02-07 22:38:37 +0530

    (fix)quiz: creation data moved to local

--- src/components/quiz/controller.js
+++ src/components/quiz/controller.js
@@ -22,7 +22,7 @@ const addQuizController = async (req, res) => {
 
   if (req.body.category) {
     apiURL += `&category=${req.body.category}`;
-    quizData["category"] = req.body.category;
+    quizData["categoryId"] = req.body.category;
   }
 
   // Hit a request
@@ -40,11 +40,13 @@ const addQuizController = async (req, res) => {
       return res.status(400).send("Contains an invalid parameter");
     }
     quizData["questions"] = data.data.results;
+    quizData["name"] = req.body.name;
+    quizData["categoryName"] = req.body.categoryName;
     const quiz = new Quiz(quizData);
 
     const quizSaved = await quiz.save();
 
-    res.json(quizSaved);
+    return res.json(quizSaved);
   } catch (error) {
     return res.status(400).send(error.message);
   }
@@ -53,9 +55,36 @@ const addQuizController = async (req, res) => {
 // Get quiz categories
 const getQuizGenDataController = async (req, res) => {
   try {
-    const data = await axios.get("https://opentdb.com/api_category.php");
+    const data =  {
+      trivia_categories: [
+        { id: 9, name: "General Knowledge" },
+        { id: 10, name: "Entertainment: Books" },
+        { id: 11, name: "Entertainment: Film" },
+        { id: 12, name: "Entertainment: Music" },
+        { id: 13, name: "Entertainment: Musicals & Theatres" },
+        { id: 14, name: "Entertainment: Television" },
+        { id: 15, name: "Entertainment: Video Games" },
+        { id: 16, name: "Entertainment: Board Games" },
+        { id: 17, name: "Science & Nature" },
+        { id: 18, name: "Science: Computers" },
+        { id: 19, name: "Science: Mathematics" },
+        { id: 20, name: "Mythology" },
+        { id: 21, name: "Sports" },
+        { id: 22, name: "Geography" },
+        { id: 23, name: "History" },
+        { id: 24, name: "Politics" },
+        { id: 25, name: "Art" },
+        { id: 26, name: "Celebrities" },
+        { id: 27, name: "Animals" },
+        { id: 28, name: "Vehicles" },
+        { id: 29, name: "Entertainment: Comics" },
+        { id: 30, name: "Science: Gadgets" },
+        { id: 31, name: "Entertainment: Japanese Anime & Manga" },
+        { id: 32, name: "Entertainment: Cartoon & Animations" },
+      ],
+    };
     const response = {
-      categories: data.data.trivia_categories,
+      categories: data.trivia_categories,
       difficulty: [
         { label: "Hard", value: "hard" },
         { label: "Medium", value: "medium" },
